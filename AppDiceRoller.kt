561f21f8114e91be11dc88bc06acf7fb754d075d
Cleiton Lima <62166572+Padawan-Dev@users.noreply.github.com>
2021-06-24 00:10:04 -0300

    Add arquvio do App feito somente em Kotlin

---
+++ AppDiceRoller.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val myFirstDice = Dice(6)
+    val diceRoll = myFirstDice.roll()
+    println("Your ${myFirstDice.numSides} sided dice rolled ${diceRoll}!")
+
+    val mySecondDice = Dice(20)
+    println("Your ${mySecondDice.numSides} sided dice rolled ${mySecondDice.roll()}!")
+}
+
+class Dice (val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
